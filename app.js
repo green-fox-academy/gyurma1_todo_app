20cf3c708907e0b22c924cffe9fef849fd404db6
gyurma1 <nemesek1@t-online.hu>
2021-11-29 14:44:53 +0100

    argumentum type mistake message

--- app.js
+++ app.js
@@ -6,48 +6,9 @@ const args = process.argv.slice(2);
 
 // console.log(process.argv,'argv');
 // console.log(args,'args');
-
-
-if (args.includes('-l')) {
-    const newTodos = new Todos();
-    newTodos.readTodos();
-    // console.log('listing');
-    // console.log(args[1]);
-    //file beolv
-    //json parsolas--rawtodolist
-    //rawtodolist---todolist=[Todo{},Todo{}]
-} else if (args.includes('-a')) {
-    const newTodos = new Todos();
-    let str = process.argv.slice(3);
-    if (str.length == 0) {
-        console.log('Nem lehetséges új feladat hozzáadása: nincs megadva feladat');
-    } else {
-        const newTodo = new Todo(str);
-        newTodos.addTodos(newTodo);
-    }
-
-    //töröl egy elemet
-} else if (args.includes('-r')) {
-    let str = process.argv.slice(3);
-    
-    if (str.length == 0) {
-        console.error('Nem lehetséges az eltávolítás:nem adott meg indexet.')
-    } else if (isNaN(str[0])) {
-
-        console.error('Nem lehetséges az eltávolítás:a megadott index nem szám.')
-    }
-    else {
-        const newTodos = new Todos();
-        newTodos.removeTodos(str);
-    }
-} else if (args.includes('-c')) {
-    //teljesít egy feladatot
-    let str = process.argv.slice(3);
-
-    const newTodos = new Todos();
-    newTodos.arrangedTodos(str);
-}
-else {
+if (!(args.includes('-l') || args.includes('-a') ||
+    args.includes('-r') || args.includes('-c'))) {
+    console.error('Nem támogatott argumentum típus.');
     console.log(`Parancssori Todo applikáció
 =============================
 Parancssori argumentumok:
@@ -55,5 +16,55 @@ Parancssori argumentumok:
     -a   Új feladatot ad hozzá
     -r   Eltávolít egy feladatot
     -c   Teljesít egy feladatot`);
+}
+else {
+    if (args.includes('-l')) {
+        const newTodos = new Todos();
+        newTodos.readTodos();
+        // console.log('listing');
+        // console.log(args[1]);
+        //file beolv
+        //json parsolas--rawtodolist
+        //rawtodolist---todolist=[Todo{},Todo{}]
+    } else if (args.includes('-a')) {
+        const newTodos = new Todos();
+        let str = process.argv.slice(3);
+        if (str.length == 0) {
+            console.log('Nem lehetséges új feladat hozzáadása: nincs megadva feladat');
+        } else {
+            const newTodo = new Todo(str);
+            newTodos.addTodos(newTodo);
+        }
 
+        //töröl egy elemet
+    } else if (args.includes('-r')) {
+        let str = process.argv.slice(3);
+
+        if (str.length == 0) {
+            console.error('Nem lehetséges az eltávolítás:nem adott meg indexet.')
+        } else if (isNaN(str[0])) {
+
+            console.error('Nem lehetséges az eltávolítás:a megadott index nem szám.')
+        }
+        else {
+            const newTodos = new Todos();
+            newTodos.removeTodos(str);
+        }
+    } else if (args.includes('-c')) {
+        //teljesít egy feladatot
+        let str = process.argv.slice(3);
+
+        const newTodos = new Todos();
+        newTodos.arrangedTodos(str);
+    }
+    else {
+        console.log(`Parancssori Todo applikáció
+=============================
+Parancssori argumentumok:
+    -l   Kilistázza a feladatokat
+    -a   Új feladatot ad hozzá
+    -r   Eltávolít egy feladatot
+    -c   Teljesít egy feladatot`);
+
+    }
 }
